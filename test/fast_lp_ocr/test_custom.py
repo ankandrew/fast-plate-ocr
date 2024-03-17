30318d459f0b7699e60371bb6435a60ba5fa449b
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-03-17 10:11:36 -0300

    Fix test

--- test/fast_lp_ocr/test_custom.py
+++ test/fast_lp_ocr/test_custom.py
@@ -2,8 +2,16 @@
 Test the custom metric/losses module.
 """
 
+# ruff: noqa: E402
+# pylint: disable=wrong-import-position,wrong-import-order,ungrouped-imports
+# fmt: off
+from fast_plate_ocr.utils import set_pytorch_backend
+
+set_pytorch_backend()
+# fmt: on
+
 import pytest
-import tensorflow as tf
+import torch
 
 from fast_plate_ocr.custom import cat_acc
 
@@ -11,9 +19,9 @@ from fast_plate_ocr.custom import cat_acc
 @pytest.mark.parametrize(
     "y_true, y_pred, expected_accuracy",
     [
-        (tf.constant([[[1, 0]] * 6]), tf.constant([[[0.9, 0.1]] * 6]), 1.0),
+        (torch.tensor([[[1, 0]] * 6]), torch.tensor([[[0.9, 0.1]] * 6]), 1.0),
     ],
 )
-def test_cat_acc(y_true: tf.Tensor, y_pred: tf.Tensor, expected_accuracy: float):
-    actual_accuracy = cat_acc(y_true, y_pred, -1, 1)
+def test_cat_acc(y_true: torch.Tensor, y_pred: torch.Tensor, expected_accuracy: float):
+    actual_accuracy = cat_acc(y_true, y_pred, 2, 1)
     assert actual_accuracy == expected_accuracy
