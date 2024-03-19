8f30c8dfd2506cb522f418fc1488f47f4988d3e8
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-03-19 18:12:37 -0300

    Use less harsh augmentation

--- fast_plate_ocr/augmentation.py
+++ fast_plate_ocr/augmentation.py
@@ -12,18 +12,17 @@ TRAIN_AUGMENTATION = A.Compose(
         A.ShiftScaleRotate(
             shift_limit=0.05,
             scale_limit=0.1,
-            rotate_limit=10,
+            rotate_limit=8,
             border_mode=cv2.BORDER_CONSTANT,
             value=BORDER_COLOR_BLACK,
             p=1,
         ),
-        A.RandomBrightnessContrast(brightness_limit=0.15, contrast_limit=0.15, p=1),
-        A.Affine(shear=5, p=1),
-        A.MotionBlur(blur_limit=(3, 7), p=0.2),
+        A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.1, p=1),
+        A.MotionBlur(blur_limit=(3, 5), p=0.1),
         A.OneOf(
             [
-                A.CoarseDropout(max_holes=10, max_height=6, max_width=6, p=0.2),
-                A.PixelDropout(dropout_prob=0.02, p=0.2),
+                A.CoarseDropout(max_holes=8, max_height=5, max_width=5, p=0.2),
+                A.PixelDropout(dropout_prob=0.01, p=0.2),
             ],
             p=0.6,
         ),
