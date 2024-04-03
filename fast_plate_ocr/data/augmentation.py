e074f13d99a910e7b52265ff89df63bc20e338a3
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-04-02 22:49:01 -0300

    Tweak augmentation

--- fast_plate_ocr/data/augmentation.py
+++ fast_plate_ocr/data/augmentation.py
@@ -10,9 +10,9 @@ BORDER_COLOR_BLACK: tuple[int, int, int] = (0, 0, 0)
 TRAIN_AUGMENTATION = A.Compose(
     [
         A.ShiftScaleRotate(
-            shift_limit=0.05,
+            shift_limit=0.06,
             scale_limit=0.1,
-            rotate_limit=8,
+            rotate_limit=9,
             border_mode=cv2.BORDER_CONSTANT,
             value=BORDER_COLOR_BLACK,
             p=1,
@@ -21,10 +21,10 @@ TRAIN_AUGMENTATION = A.Compose(
         A.MotionBlur(blur_limit=(3, 5), p=0.1),
         A.OneOf(
             [
-                A.CoarseDropout(max_holes=8, max_height=5, max_width=5, p=0.2),
+                A.CoarseDropout(max_holes=10, max_height=4, max_width=4, p=0.3),
                 A.PixelDropout(dropout_prob=0.01, p=0.2),
             ],
-            p=0.6,
+            p=0.7,
         ),
     ]
 )
