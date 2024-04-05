1a5f57470113e5edc6645d5e6190037030efdcee
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-04-05 12:46:32 -0300

    Tweak HUB module

--- fast_plate_ocr/inference/hub.py
+++ fast_plate_ocr/inference/hub.py
@@ -10,12 +10,22 @@ from http import HTTPStatus
 
 from tqdm.asyncio import tqdm
 
+from fast_plate_ocr.inference.utils import safe_write
+
 BASE_URL: str = "https://github.com/ankandrew/cnn-ocr-lp/releases/download"
 
-AVAILABLE_ONNX_MODELS: dict[str, str] = {
-    "argentinian-plates-cnn-model": f"{BASE_URL}/untagged-2d82907046a6a4abe03f/arg-cnn-ocr.onnx"
+AVAILABLE_ONNX_MODELS: dict[str, tuple[str, str]] = {
+    "argentinian-plates-cnn-model": (
+        # f"{BASE_URL}/untagged-2d82907046a6a4abe03f/arg-cnn-ocr.onnx",
+        # f"{BASE_URL}/untagged-2d82907046a6a4abe03f/arg-cnn-ocr-config.yaml",
+        "https://filebin.net/ulk7b5jn2o3dcg9a/arg_cnn_ocr.onnx",
+        "https://filebin.net/acbb0088wegfsdtp/arg_cnn_ocr_config.yaml",
+    )
 }
 """Available ONNX models for doing inference."""
+
+MODEL_CACHE_DIR: pathlib.Path = pathlib.Path.home() / ".cache" / "fast-plate-ocr"
+"""Default location where models will be stored."""
 
 
 def _download_with_progress(url: str, filename: pathlib.Path) -> None:
@@ -25,7 +35,7 @@ def _download_with_progress(url: str, filename: pathlib.Path) -> None:
     :param url: URL of the model to download.
     :param filename: Where to save the OCR model.
     """
-    with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
+    with urllib.request.urlopen(url) as response, safe_write(filename, mode="wb") as out_file:
         if response.getcode() != HTTPStatus.OK:
             raise ValueError(f"Failed to download file from {url}. Status code: {response.status}")
 
@@ -38,38 +48,50 @@ def _download_with_progress(url: str, filename: pathlib.Path) -> None:
 
 def download_model(
     model_name: str,
-    save_directory: pathlib.Path,
-    skip_if_exists: bool = True,
-    create_dir: bool = True,
-) -> pathlib.Path:
+    save_directory: pathlib.Path | None = None,
+    force_download: bool = False,
+) -> tuple[pathlib.Path, pathlib.Path]:
     """
-    Download an OCR model to a given directory.
+    Download an OCR model and the config to a given directory.
 
     :param model_name: Which model to download. Current available models are
      ('argentinian-plates-cnn-model', ).
-    :param save_directory: Directory to save the OCR model. It should point to a folder.
-    :param skip_if_exists: Don't download model if it already exists in `save_directory`.
-    :param create_dir: Create directory recursively if parents don't exist.
-    :return: Path to the downloaded model.
+    :param save_directory: Directory to save the OCR model. It should point to a folder. If not
+     supplied, this will point to '~/.cache/<model_name>'
+    :param force_download: Force and download the model if it already exists in `save_directory`.
+    :return: A tuple consisting of (model_downloaded_path, config_downloaded_path).
     """
     if model_name not in AVAILABLE_ONNX_MODELS:
-        raise ValueError(f"Unknown model {model_name}. Use one of {AVAILABLE_ONNX_MODELS.keys()}")
+        available_models = ", ".join(AVAILABLE_ONNX_MODELS.keys())
+        raise ValueError(f"Unknown model {model_name}. Use one of [{available_models}]")
 
-    model_url = AVAILABLE_ONNX_MODELS[model_name]
-    filename = save_directory / model_url.split("/")[-1]
+    if save_directory is None:
+        save_directory = MODEL_CACHE_DIR / model_name
+    elif save_directory.is_file():
+        raise ValueError(f"Expected a directory, but got {save_directory}")
 
-    if skip_if_exists and filename.is_file():
+    save_directory.mkdir(parents=True, exist_ok=True)
+
+    model_url, config_url = AVAILABLE_ONNX_MODELS[model_name]
+    model_filename = save_directory / model_url.split("/")[-1]
+    config_filename = save_directory / config_url.split("/")[-1]
+
+    if not force_download and model_filename.is_file() and config_filename.is_file():
         logging.info(
-            "Skipping download of %s model, already exists at %s",
+            "Skipping download of '%s' model, already exists at %s",
             model_name,
             save_directory,
         )
-        return filename
+        return model_filename, config_filename
 
-    if save_directory.is_file():
-        raise ValueError(f"Expected a directory, but got {save_directory}")
+    # Download the model if not present or if we want to force the download
+    if force_download or not model_filename.is_file():
+        logging.info("Downloading model to %s", model_filename)
+        _download_with_progress(url=model_url, filename=model_filename)
 
-    save_directory.parent.mkdir(parents=create_dir, exist_ok=True)
+    # Same for the config
+    if force_download or not config_filename.is_file():
+        logging.info("Downloading config to %s", config_filename)
+        _download_with_progress(url=config_url, filename=config_filename)
 
-    logging.info("Downloading model to %s", filename)
-    _download_with_progress(url=model_url, filename=filename)
+    return model_filename, config_filename
