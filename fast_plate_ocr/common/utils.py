c1c9f87552f0861b10ffea5f99dcf764ba5cee2d
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-04-06 13:27:12 -0300

    Improve logging in log_time_taken

--- fast_plate_ocr/common/utils.py
+++ fast_plate_ocr/common/utils.py
@@ -10,11 +10,20 @@ from contextlib import contextmanager
 
 @contextmanager
 def log_time_taken(process_name: str) -> Iterator[None]:
-    """A concise context manager to time code snippets and log the result."""
+    """
+    A concise context manager to time code snippets and log the result.
+
+    Usage:
+    with log_time_taken("process_name"):
+        # Code snippet to be timed
+
+    :param process_name: Name of the process being timed.
+    """
     time_start: float = time.perf_counter()
     try:
         yield
     finally:
         time_end: float = time.perf_counter()
         time_elapsed: float = time_end - time_start
-        logging.info("Computation time of '%s' = %.3fms", process_name, 1000 * time_elapsed)
+        logger = logging.getLogger(__name__)
+        logger.info("Computation time of '%s' = %.3fms", process_name, 1000 * time_elapsed)
