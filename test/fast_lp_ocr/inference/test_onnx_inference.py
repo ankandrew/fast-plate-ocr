e23b24e700be8cc76232c313dfc1a5fe16306b06
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-04-08 15:47:30 -0300

    Initial test for ONNX inference

---
+++ test/fast_lp_ocr/inference/test_onnx_inference.py
@@ -0,0 +1,48 @@
+"""
+Tests for ONNX inference module.
+"""
+
+from collections.abc import Iterator
+from test.assets import ASSETS_DIR
+
+import cv2
+import numpy.typing as npt
+import pytest
+
+from fast_plate_ocr import ONNXPlateRecognizer
+
+
+@pytest.fixture(scope="module")
+def onnx_model() -> Iterator[ONNXPlateRecognizer]:
+    yield ONNXPlateRecognizer("argentinian-plates-cnn-model", device="cpu")
+
+
+@pytest.mark.parametrize(
+    "input_image, expected_result",
+    [
+        # Single image path
+        (str(ASSETS_DIR / "test_plate_1.png"), 1),
+        # Multiple Image paths
+        (
+            [str(ASSETS_DIR / "test_plate_1.png"), str(ASSETS_DIR / "test_plate_2.png")],
+            2,
+        ),
+        # NumPy array with single image
+        (cv2.imread(str(ASSETS_DIR / "test_plate_1.png"), cv2.IMREAD_GRAYSCALE), 1),
+        # NumPy array with batch images
+        (
+            [
+                cv2.imread(str(ASSETS_DIR / "test_plate_1.png"), cv2.IMREAD_GRAYSCALE),
+                cv2.imread(str(ASSETS_DIR / "test_plate_2.png"), cv2.IMREAD_GRAYSCALE),
+            ],
+            2,
+        ),
+    ],
+)
+def test_result_from_different_image_sources(
+    input_image: str | list[str] | npt.NDArray,
+    expected_result: int,
+    onnx_model: ONNXPlateRecognizer,
+) -> None:
+    actual_result = len(onnx_model.run(input_image))
+    assert actual_result == expected_result
