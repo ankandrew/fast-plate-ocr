280a99536c9fa43d7fedac852409ae37b3174de6
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-05-01 15:01:53 -0300

    Optimize and use numpy indexing instead of pandas iloc

--- fast_plate_ocr/train/data/dataset.py
+++ fast_plate_ocr/train/data/dataset.py
@@ -6,6 +6,7 @@ import os
 from os import PathLike
 
 import albumentations as A
+import numpy.typing as npt
 import pandas as pd
 from torch.utils.data import Dataset
 
@@ -19,31 +20,30 @@ class LicensePlateDataset(Dataset):
         annotations_file: str | PathLike[str],
         config: PlateOCRConfig,
         transform: A.Compose | None = None,
-    ):
-        self.annotations = pd.read_csv(annotations_file)
-        self.annotations["image_path"] = (
-            os.path.dirname(os.path.realpath(annotations_file))
-            + os.sep
-            + self.annotations["image_path"]
+    ) -> None:
+        annotations = pd.read_csv(annotations_file)
+        annotations["image_path"] = (
+            os.path.dirname(os.path.realpath(annotations_file)) + os.sep + annotations["image_path"]
         )
         assert (
-            self.annotations["plate_text"].str.len() <= config.max_plate_slots
+            annotations["plate_text"].str.len() <= config.max_plate_slots
         ).all(), "Plates are longer than max_plate_slots specified param. Change the parameter."
+        self.annotations = annotations.to_numpy()
         self.config = config
         self.transform = transform
 
-    def __len__(self):
-        return len(self.annotations.index)
+    def __len__(self) -> int:
+        return self.annotations.shape[0]
 
-    def __getitem__(self, idx):
-        annotation = self.annotations.iloc[idx]
+    def __getitem__(self, idx) -> tuple[npt.NDArray, npt.NDArray]:
+        image_path, plate_text = self.annotations[idx]
         x = utils.read_plate_image(
-            image_path=annotation.image_path,
+            image_path=image_path,
             img_height=self.config.img_height,
             img_width=self.config.img_width,
         )
         y = utils.target_transform(
-            plate_text=annotation.plate_text,
+            plate_text=plate_text,
             max_plate_slots=self.config.max_plate_slots,
             alphabet=self.config.alphabet,
             pad_char=self.config.pad_char,
