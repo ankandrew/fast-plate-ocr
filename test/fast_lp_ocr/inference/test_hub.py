85e5882069bb885704fb1084e1c060504b4410c0
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-10-14 00:05:12 -0300

    Test that model weights/configs are accessible

---
+++ test/fast_lp_ocr/inference/test_hub.py
@@ -0,0 +1,24 @@
+"""
+Tests for ONNX hub module.
+"""
+
+from http import HTTPStatus
+
+import pytest
+import requests
+
+from fast_plate_ocr.inference.hub import AVAILABLE_ONNX_MODELS
+
+
+@pytest.mark.parametrize("model_name", AVAILABLE_ONNX_MODELS.keys())
+def test_model_and_config_urls(model_name):
+    """
+    Test to check if the model and config URLs for AVAILABLE_ONNX_MODELS are valid.
+    """
+    model_url, config_url = AVAILABLE_ONNX_MODELS[model_name]
+
+    for url in [model_url, config_url]:
+        response = requests.get(url, timeout=5, allow_redirects=True)
+        assert (
+            response.status_code == HTTPStatus.OK
+        ), f"URL {url} is not accessible, got {response.status_code}"
