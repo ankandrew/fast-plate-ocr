62c9f1448b61a5ee64d7edb4d9a0a588070f63e5
ankandrew <61120139+ankandrew@users.noreply.github.com>
2025-04-12 13:10:41 -0300

    Add test to ensure train/inference configs match

--- test/fast_lp_ocr/train/test_config.py
+++ test/fast_lp_ocr/train/test_config.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 import pytest
 
-from fast_plate_ocr.train.model.config import PlateOCRConfig, load_config_from_yaml
+from fast_plate_ocr.inference.config import PlateOCRConfig as PlateOCRConfigTypedDict
+from fast_plate_ocr.train.model.config import PlateOCRConfig as PlateOCRConfigModel
+from fast_plate_ocr.train.model.config import load_config_from_yaml
 from test import PROJECT_ROOT_DIR
 
 
@@ -33,4 +35,10 @@ def test_yaml_configs(file_path: Path) -> None:
 )
 def test_invalid_config_raises(raw_config: dict) -> None:
     with pytest.raises(ValueError):
-        PlateOCRConfig(**raw_config)
+        PlateOCRConfigModel(**raw_config)
+
+
+def test_configs_are_consistent():
+    typed_dict_fields = set(PlateOCRConfigTypedDict.__annotations__.keys())
+    pydantic_fields = set(PlateOCRConfigModel.model_fields.keys())
+    assert typed_dict_fields == pydantic_fields, "Train and inference config have different fields"
