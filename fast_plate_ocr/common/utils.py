c485b33812fc3799507ff7702e2f42cf7933b3d0
ankandrew <61120139+ankandrew@users.noreply.github.com>
2025-04-20 20:21:02 -0300

    Move safe_write context manager to common.utils module

--- fast_plate_ocr/common/utils.py
+++ fast_plate_ocr/common/utils.py
@@ -3,9 +3,12 @@ Common utilities used across the package.
 """
 
 import logging
+import os
 import time
 from collections.abc import Callable, Iterator
 from contextlib import contextmanager
+from pathlib import Path
+from typing import IO, Any
 
 
 @contextmanager
@@ -42,3 +45,24 @@ def measure_time() -> Iterator[Callable[[], float]]:
     start_time = end_time = time.perf_counter()
     yield lambda: (end_time - start_time) * 1_000
     end_time = time.perf_counter()
+
+
+@contextmanager
+def safe_write(
+    file: str | os.PathLike[str],
+    mode: str = "wb",
+    encoding: str | None = None,
+    **kwargs: Any,
+) -> Iterator[IO]:
+    """
+    Context manager for safe file writing.
+
+    Opens the specified file for writing and yields a file object.
+    If an exception occurs during writing, the file is removed before raising the exception.
+    """
+    try:
+        with open(file, mode, encoding=encoding, **kwargs) as f:
+            yield f
+    except Exception as e:
+        Path(file).unlink(missing_ok=True)
+        raise e
