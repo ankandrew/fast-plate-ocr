f6b80987d1d59937f51db0e3ce3e55f97f2179a3
ankandrew <61120139+ankandrew@users.noreply.github.com>
2025-05-09 23:24:46 -0300

    Add test for top_3_k_metric

--- test/fast_lp_ocr/train/test_metric.py
+++ test/fast_lp_ocr/train/test_metric.py
@@ -9,6 +9,7 @@ from fast_plate_ocr.train.model.metric import (
     cat_acc_metric,
     plate_acc_metric,
     plate_len_acc_metric,
+    top_3_k_metric,
 )
 
 
@@ -79,6 +80,70 @@ def test_plate_accuracy(y_true: np.ndarray, y_pred: np.ndarray, expected_accurac
 
 
 @pytest.mark.parametrize(
+    "y_true, y_pred, vocab_size, expected_acc",
+    [
+        # Both true labels are within the top-3 predictions, so accuracy should be 1.0
+        (
+            np.array(
+                [
+                    [1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 1],
+                ],
+            ),
+            np.array(
+                [
+                    [0.30, 0.20, 0.10, 0.35, 0.05],
+                    [0.10, 0.05, 0.15, 0.20, 0.50],
+                ],
+            ),
+            5,
+            1.0,
+        ),
+        # One true label is in the top-3 predictions, the other one not, so accuracy should be 0.5
+        (
+            np.array(
+                [
+                    [1, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                ],
+            ),
+            np.array(
+                [
+                    [0.40, 0.30, 0.20, 0.10, 0.00],
+                    [0.05, 0.15, 0.10, 0.50, 0.20],
+                ],
+            ),
+            5,
+            0.5,
+        ),
+        # Neither true label is in the top-3 predictions, so accuracy should be 0.0
+        (
+            np.array(
+                [
+                    [0, 1, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                ],
+            ),
+            np.array(
+                [
+                    [0.50, 0.05, 0.02, 0.25, 0.18],
+                    [0.60, 0.20, 0.05, 0.10, 0.15],
+                ],
+            ),
+            5,
+            0.0,
+        ),
+    ],
+)
+def test_top_3_k(
+    y_true: np.ndarray, y_pred: np.ndarray, vocab_size: int, expected_acc: float
+) -> None:
+    metric_fn = top_3_k_metric(vocab_size)
+    actual = metric_fn(y_true, y_pred)
+    assert actual == expected_acc
+
+
+@pytest.mark.parametrize(
     "y_true, y_pred, max_slots, vocab_size, pad_idx, expected_acc",
     [
         # All lengths match, accuracy should be 1.0
@@ -120,7 +185,7 @@ def test_plate_accuracy(y_true: np.ndarray, y_pred: np.ndarray, expected_accurac
             0,
             1.0,
         ),
-        # 2) One match, one mismatch, accuracy should be 0.5
+        # One match, one mismatch, accuracy should be 0.5
         (
             np.array(
                 [
