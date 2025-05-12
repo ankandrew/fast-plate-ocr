53af1beb7cc7f83c32af928d6a4156d818b96515
ankandrew <61120139+ankandrew@users.noreply.github.com>
2025-05-12 20:39:57 -0300

    Add new tests for reading configs for inference

--- test/fast_lp_ocr/train/test_config.py
+++ test/fast_lp_ocr/train/test_config.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 import pytest
 
-from fast_plate_ocr.inference.config import PlateOCRConfig as PlateOCRConfigTypedDict
-from fast_plate_ocr.train.model.config import PlateOCRConfig as PlateOCRConfigModel
+from fast_plate_ocr.inference.config import PlateOCRConfig as PlateOCRConfigInference
+from fast_plate_ocr.train.model.config import PlateOCRConfig as PlateOCRConfigTrain
 from fast_plate_ocr.train.model.config import load_config_from_yaml
 from test import PROJECT_ROOT_DIR
 
@@ -16,8 +16,16 @@ from test import PROJECT_ROOT_DIR
     "file_path",
     [f for f in PROJECT_ROOT_DIR.joinpath("config").iterdir() if f.suffix in (".yaml", ".yml")],
 )
-def test_yaml_configs(file_path: Path) -> None:
+def test_yaml_configs_are_valid(file_path: Path) -> None:
     load_config_from_yaml(file_path)
+
+
+@pytest.mark.parametrize(
+    "file_path",
+    [f for f in PROJECT_ROOT_DIR.joinpath("config").iterdir() if f.suffix in (".yaml", ".yml")],
+)
+def test_yaml_configs_for_inference_are_valid(file_path: Path) -> None:
+    PlateOCRConfigInference.from_yaml(file_path)
 
 
 @pytest.mark.parametrize(
@@ -35,10 +43,10 @@ def test_yaml_configs(file_path: Path) -> None:
 )
 def test_invalid_config_raises(raw_config: dict) -> None:
     with pytest.raises(ValueError):
-        PlateOCRConfigModel(**raw_config)
+        PlateOCRConfigTrain(**raw_config)
 
 
 def test_configs_are_consistent():
-    typed_dict_fields = set(PlateOCRConfigTypedDict.__annotations__.keys())
-    pydantic_fields = set(PlateOCRConfigModel.model_fields.keys())
+    typed_dict_fields = set(PlateOCRConfigInference.__annotations__.keys())
+    pydantic_fields = set(PlateOCRConfigTrain.model_fields.keys())
     assert typed_dict_fields == pydantic_fields, "Train and inference config have different fields"
