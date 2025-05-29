054f016ef1d2198b7df4f20cbadaadfb7354a65f
ankandrew <61120139+ankandrew@users.noreply.github.com>
2025-05-29 10:59:29 -0300

    Init `build_cct_model`

--- fast_plate_ocr/train/model/models.py
+++ fast_plate_ocr/train/model/models.py
@@ -2,91 +2,76 @@
 Model definitions for the FastLP OCR.
 """
 
-from typing import Literal
+from collections.abc import Sequence
 
-from keras.src.activations import softmax
-from keras.src.layers import (
-    Activation,
-    Concatenate,
-    Dense,
-    Dropout,
-    GlobalAveragePooling2D,
-    Input,
-    Rescaling,
-    Reshape,
-    Softmax,
+import keras
+import numpy as np
+from keras import layers
+
+from fast_plate_ocr.train.model.layers import (
+    PositionEmbedding,
+    TokenReducer,
+    TransformerBlock,
+    VocabularyProjection,
 )
-from keras.src.models import Model
-
-from fast_plate_ocr.train.model.layer_blocks import (
-    block_average_conv_down,
-    block_bn,
-    block_max_conv_down,
-    block_no_activation,
-)
+from fast_plate_ocr.train.model.model_builder import CCTModelConfig, LayerConfig
 
 
-def cnn_ocr_model(
-    h: int,
-    w: int,
+def build_stem_from_config(specs: Sequence[LayerConfig]) -> keras.Sequential:
+    return keras.Sequential([spec.to_keras_layer() for spec in specs], name="conv_stem")
+
+
+def build_cct_model(
+    cfg: CCTModelConfig,
+    input_shape: tuple[int, int, int],
     max_plate_slots: int,
     vocabulary_size: int,
-    dense: bool = True,
-    activation: str = "relu",
-    pool_layer: Literal["avg", "max"] = "max",
-) -> Model:
-    """
-    OCR model implemented with just CNN layers (v2).
-    """
-    input_tensor = Input((h, w, 1))
-    x = Rescaling(1.0 / 255)(input_tensor)
-    # Pooling-Conv layer
-    if pool_layer == "avg":
-        block_pool_conv = block_average_conv_down
-    elif pool_layer == "max":
-        block_pool_conv = block_max_conv_down
-    # Backbone
-    x = block_pool_conv(x, n_c=32, padding="same", activation=activation)
-    x, _ = block_bn(x, k=3, n_c=64, s=1, padding="same", activation=activation)
-    x, _ = block_bn(x, k=1, n_c=64, s=1, padding="same", activation=activation)
-    x = block_pool_conv(x, n_c=64, padding="same", activation=activation)
-    x, _ = block_bn(x, k=3, n_c=128, s=1, padding="same", activation=activation)
-    x, _ = block_bn(x, k=1, n_c=128, s=1, padding="same", activation=activation)
-    x = block_pool_conv(x, n_c=128, padding="same", activation=activation)
-    x, _ = block_bn(x, k=3, n_c=128, s=1, padding="same", activation=activation)
-    x, _ = block_bn(x, k=1, n_c=256, s=1, padding="same", activation=activation)
-    x = block_pool_conv(x, n_c=256, padding="same", activation=activation)
-    x, _ = block_bn(x, k=1, n_c=512, s=1, padding="same", activation=activation)
-    x, _ = block_bn(x, k=1, n_c=1024, s=1, padding="same", activation=activation)
-    x = (
-        head(x, max_plate_slots, vocabulary_size)
-        if dense
-        else head_no_fc(x, max_plate_slots, vocabulary_size)
+) -> keras.Model:
+    # 1. Input
+    inputs = layers.Input(shape=input_shape)
+
+    # 2. Rescale & conv stem
+    data_rescale = cfg.rescaling.to_keras_layer()
+    x = build_stem_from_config(cfg.tokenizer.blocks)(data_rescale(inputs))
+
+    # 3. Flatten and add positional embedding (if applicable)
+    h, w = x.shape[1:3]  # static dims after conv stem
+    if h is None or w is None:
+        raise ValueError("Conv-stem must produce fixed spatial dims to add position embeddings.")
+    # (B, H, W, C) -> (B, H*W, C)
+    x = layers.Reshape((-1, x.shape[-1]))(x)
+
+    if cfg.tokenizer.positional_emb:
+        seq_len = h * w
+        x += PositionEmbedding(sequence_length=seq_len)(x)
+
+    # 4. N x TransformerBlock's
+    dpr = list(
+        np.linspace(0.0, cfg.transformer_encoder.stochastic_depth, cfg.transformer_encoder.layers)
     )
-    return Model(inputs=input_tensor, outputs=x)
+    for i, rate in enumerate(dpr, 1):
+        x = TransformerBlock(
+            projection_dim=cfg.transformer_encoder.projection_dim,
+            num_heads=cfg.transformer_encoder.heads,
+            mlp_units=cfg.transformer_encoder.units,
+            attention_dropout=cfg.transformer_encoder.attention_dropout,
+            mlp_dropout=cfg.transformer_encoder.mlp_dropout,
+            drop_path_rate=rate,
+            norm_type=cfg.transformer_encoder.normalization,
+            name=f"transformer_block_{i}",
+        )(x)
 
+    # 5. Reduce to a fixed number of tokens, then project to vocab
+    x = TokenReducer(
+        num_tokens=max_plate_slots,
+        projection_dim=cfg.transformer_encoder.projection_dim,
+        num_heads=cfg.transformer_encoder.token_reducer_heads,
+    )(x)
 
-def head(x, max_plate_slots: int, vocabulary_size: int):
-    """
-    Model's head with Fully Connected (FC) layers.
-    """
-    x = GlobalAveragePooling2D()(x)
-    # dropout for more robust learning
-    x = Dropout(0.5)(x)
-    dense_outputs = [
-        Activation(softmax)(Dense(units=vocabulary_size)(x)) for _ in range(max_plate_slots)
-    ]
-    # concat all the dense outputs
-    x = Concatenate()(dense_outputs)
-    return x
+    logits = VocabularyProjection(
+        vocabulary_size=vocabulary_size,
+        dropout_rate=cfg.transformer_encoder.head_mlp_dropout,
+        name="vocab_projection",
+    )(x)
 
-
-def head_no_fc(x, max_plate_slots: int, vocabulary_size: int):
-    """
-    Model's head without Fully Connected (FC) layers.
-    """
-    x = block_no_activation(x, k=1, n_c=max_plate_slots * vocabulary_size, s=1, padding="same")
-    x = GlobalAveragePooling2D()(x)
-    x = Reshape((max_plate_slots, vocabulary_size, 1))(x)
-    x = Softmax(axis=-2)(x)
-    return x
+    return keras.Model(inputs, logits, name="CCT_OCR")
